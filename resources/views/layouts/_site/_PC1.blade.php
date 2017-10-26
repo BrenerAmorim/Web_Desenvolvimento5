7b82e5bf8e8145c1c9ade8eb620ac3560bb2c873
zotrovisk <leo.junior.ribeiro@gmail.com>
2017-10-26 10:14:51 -0200

    arquivo

---
+++ resources/views/layouts/_site/_PC1.blade.php
@@ -0,0 +1,44 @@
+<div id="fotos">
+
+<img class="materialboxed" width="500" src="img/pc1.png">
+
+</div>
+
+        <footer id="footer" >
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h1 class="black-text">Modelo:Dell Inspiron 15 5000</h1>
+                
+              </div>
+              <div class="col l4 offset-l2 s12">
+                 <h4 class="black-text">Conexões:HDMI USB</h4>
+                 <h4 class="black-text">Placa de Vídeo: Nvidia Geforce Gtx 980 3xsli Waterforce 4gb Gddr5 256 Bits - Gv-N980x3wa-4gd</h4>
+                 <h4 class="black-text">HD: 1 Terabyte</h4>
+                 <h4 class="black-text">Monitor: 15,6 Polegadas</h4>
+                 <h4 class="black-text"<p>Menor Preço: <a href="https://www.submarino.com.br">Submarino</a></h4>
+                 <h4 class="black-text">Avaliação: 7.8</h4>
+                 <h4 class="black-text">Finalidade:Programador</h4> 
+              </div>
+            </div>
+          </div>
+          
+        </footer>
+
+<div id="mainDescDiv" class="container">
+<div id="pcProp" >
+<p>
+	<h3>Modelo:Dell Inspiron 15 5000</h3><br>
+	<h6>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h6><br>
+    <h6>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h6><br>
+    <h6>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h6><br>
+    <h6>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h6><br>
+    <h6>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h6><br>
+    <h6>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h6><br>
+    <h6>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h6><br>
+    <h6>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h6><br>
+</p>
+</div>
+</div>
+
+</div>
